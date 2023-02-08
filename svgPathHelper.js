18987fb82e421d962b48ff823337c1d8323d75d4
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2023-02-08 15:04:19 +0100

    [WIP] Test Version 2

--- svgPathHelper.js
+++ svgPathHelper.js
@@ -1,5 +1,8 @@
-export default svgPathHelper
-{
+export default function drawLine(x1,y1,x2,y2){
+    return `<line x1="${randomIntFromInterval(-40,500)}"  y1="${y1}" x2="${x2}" y2="${y2}" stroke="red"></line>`
+}
 
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
