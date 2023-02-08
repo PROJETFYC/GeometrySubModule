8bc64bbd008f688a9808d76a42a1b52a4e6051bf
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2023-02-08 15:12:48 +0100

    [WIP] Test Version 4

--- svgPathHelper.js
+++ svgPathHelper.js
@@ -2,7 +2,7 @@ export default function drawLine(x1,y1,x2,y2){
     const rayon = randomIntFromInterval(20,80)
     const start =  randomIntFromInterval(-40,500)
 
-    return `<path d="M ${start } ${start } A ${rayon } ${rayon } 0 0 1 ${rayon * 2 } ${rayon * 2 }"  d="" stroke="red"/>`
+    return `<path d="M ${start } ${start } C ${x1} ${y1} ${x2} ${y2} ${x2} ${y2}"  d="" stroke="red"/>`
 
 }
 
